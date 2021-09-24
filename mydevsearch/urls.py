26552f7ea8586958b21c7b8714f1156e9149cf69
prashant <prashant.gupta@go-mmt.com>
2021-09-24 17:19:58 +0530

    added users

--- mydevsearch/urls.py
+++ mydevsearch/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(urls)),
-    path('users/', include(user_urls))
+    path('', include(user_urls)),
+    path('projects/', include(urls))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
